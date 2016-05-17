d0894d14eb9bf5debf394384150b9828e39b7e5c
Stephen Lautier <stephen.lautier@outlook.com>
2016-05-17 02:26:28 +0200

    removed typings and installed `ssv-ng2-core`

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -2,4 +2,3 @@
 /// <reference path="browser/ambient/jasmine/index.d.ts" />
 /// <reference path="browser/ambient/lodash/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/definitions/ssv-ng2-core/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -2,4 +2,3 @@
 /// <reference path="main/ambient/jasmine/index.d.ts" />
 /// <reference path="main/ambient/lodash/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/definitions/ssv-ng2-core/index.d.ts" />
