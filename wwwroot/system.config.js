f7df2be5e0ef6211c110d8666e956f5ba6f363b4
Stephen Lautier <stephen.lautier@outlook.com>
2016-06-03 01:26:11 +0200

    installed ssv-core + lodash + font-awesome

--- wwwroot/system.config.js
+++ wwwroot/system.config.js
@@ -1,74 +1,80 @@
 /*
- * This config is only used during development and build phase only
- * It will not be available on production
- *
- */
+* This config is only used during development and build phase only
+* It will not be available on production
+*
+*/
 
-(function(global) {
-    // wildcard paths
-    var paths = {
-      "*": "dist/*",
-        "n:*": "node_modules/*"
-    };
+(function (global) {
+	// wildcard paths
+	var paths = {
+		"*": "dist/*",
+		"n:*": "node_modules/*"
+	};
 
-    // map tells the System loader where to look for things
-    var map = {
-        "app": "app",
-        "test": "test",
-        "rxjs": "n:rxjs",
-        "@angular": "n:@angular",
-        "angular2-in-memory-web-api": "n:angular2-in-memory-web-api",
-        "ssv-ng2-core": "n:ssv-ng2-core",
-        "lodash": "n:lodash"
-    };
+	// map tells the System loader where to look for things
+	var map = {
+		"app": "app",
+		"test": "test",
+		"rxjs": "n:rxjs",
+		"@angular": "n:@angular",
+		"@ngrx": "n:@ngrx",
+		"angular2-in-memory-web-api": "n:angular2-in-memory-web-api",
+		"ssv-core": "n:ssv-core",
+		"ssv-ng2-core": "n:ssv-ng2-core",
+		"lodash": "n:lodash"
+	};
 
-    // packages tells the System loader how to load when no filename and/or no extension
-    var packages = {
-        "app": {
-            defaultExtension: "js"
-        },
-        "test": {
-            defaultExtension: "js"
-        },
-        "rxjs": {
-            defaultExtension: "js"
-        },        
-        "angular2-in-memory-web-api": {
-            defaultExtension: "js"
-        },
-        "ssv-ng2-core": {
-            main: "_artifact/amd/index.js",
-            defaultExtension: "js"
-        }
-    };
+	// packages tells the System loader how to load when no filename and/or no extension
+	var packages = {
+		"app": {
+			defaultExtension: "js"
+		},
+		"test": {
+			defaultExtension: "js"
+		},
+		"rxjs": {
+			defaultExtension: "js"
+		},
+		"angular2-in-memory-web-api": {
+			defaultExtension: "js"
+		},
+		"ssv-core": {
+			main: "dist/amd/index.js",
+			defaultExtension: "js"
+		},
+		"ssv-ng2-core": {
+			main: "_artifact/amd/index.js",
+			defaultExtension: "js"
+		}
+	};
 
-    var packageNames = [
-        "@angular/common",
-        "@angular/compiler",
-        "@angular/core",
-        "@angular/http",
-        "@angular/platform-browser",
-        "@angular/platform-browser-dynamic",
-        "@angular/router",
-        "@angular/router-deprecated",
-        "@angular/testing",
-        "lodash"
-    ];
+	var packageNames = [
+		"@angular/common",
+		"@angular/compiler",
+		"@angular/core",
+		"@angular/http",
+		"@angular/platform-browser",
+		"@angular/platform-browser-dynamic",
+		"@angular/router",
+		"@angular/router-deprecated",
+		"@angular/testing",
+		"lodash"
+	];
 
-    // add package entries for angular packages in the form "@angular/common": { main: "index.js", defaultExtension: "js" }
-    packageNames.forEach(function(pkgName) {
-        packages[pkgName] = { main: "index.js", defaultExtension: "js" };
-    });
+	// add package entries for angular packages in the form "@angular/common": { main: "index.js", defaultExtension: "js" }
+	packageNames.forEach(function (pkgName) {
+		packages[pkgName] = { main: "index.js", defaultExtension: "js" };
+	});
 
-    var config = {
-        map: map,
-        packages: packages,
-        paths: paths
-    };
+	var config = {
+		map: map,
+		packages: packages,
+		paths: paths
+	};
 
-    // filterSystemConfig - index.html"s chance to modify config before we register it.
-    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+	// filterSystemConfig - index.html"s chance to modify config before we register it.
+	if (global.filterSystemConfig) { global.filterSystemConfig(config); }
 
-    System.config(config);
+	System.config(config);
 
 })(this);
