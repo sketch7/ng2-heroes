91f343ab5cd4a0fe5e30762d0c3461b8ce5dc25a
Stephen Lautier <stephen.lautier@outlook.com>
2016-06-12 18:21:08 +0200

    chore(typings): reinstalled typings using npm instead of global

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
