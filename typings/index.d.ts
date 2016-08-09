02ac9ef8f14ebb45820aab90c5fa2fb659602406
Stephen Lautier <stephen.lautier@outlook.com>
2016-08-09 23:49:07 +0200

    feat(packages): updated angular to rc5 + changed es6-shim to core-js

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="custom.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="modules/fuse/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
