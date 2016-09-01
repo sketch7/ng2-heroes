5d5b7c23f0cc7d89864c4f21bf988460101c2941
Stephen Lautier <stephen.lautier@outlook.com>
2016-09-01 22:47:13 +0200

    refactor hero effect as per ngrx/effects 2.x

--- src/app/areas/hero/hero.effect.ts
+++ src/app/areas/hero/hero.effect.ts
@@ -1,25 +1,24 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/Observable";
-import {Effect, StateUpdates} from "@ngrx/effects";
-import {Action} from "@ngrx/store";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Effect, Actions } from "@ngrx/effects";
+import { Action } from "@ngrx/store";
 
-import {AppState} from "../../app.state";
-import {HeroAction, heroAction} from "./hero.action";
-import {HeroService} from "./hero.service";
+import { HeroAction, heroAction } from "./hero.action";
+import { HeroService } from "./hero.service";
 
 
 @Injectable()
 export class HeroEffect {
 
-	@Effect() fetchAll$: Observable<Action> = this.updates$
-		.whenAction(heroAction.fetchAll)
+	@Effect() fetchAll$: Observable<Action> = this.actions$
+		.ofType(heroAction.fetchAll)
 		.switchMap(() => this.service.getAll()
 			.map(result => this.action.fetchAllSuccess(result))
 			.catch(() => Observable.of(this.action.fetchAllFail()))
 		);
 
 	constructor(
-		private updates$: StateUpdates<AppState>,
+		private actions$: Actions,
 		private action: HeroAction,
 		private service: HeroService
 	) {
