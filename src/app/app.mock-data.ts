cd0c6cc0b435e6987a7cda1d74d0701bf063bb8f
Stephen Lautier <stephen.lautier@outlook.com>
2016-09-01 21:48:04 +0200

    updated  "angular2-in-memory-web-api"

--- src/app/app.mock-data.ts
+++ src/app/app.mock-data.ts
@@ -1,5 +1,5 @@
 import {heroes} from "./areas/index";
-import {InMemoryDbService} from "angular2-in-memory-web-api/core";
+import {InMemoryDbService} from "angular2-in-memory-web-api";
 
 export class MockAppData implements InMemoryDbService {
 	createDb() {
